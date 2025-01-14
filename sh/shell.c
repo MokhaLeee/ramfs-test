31c2a04ebc3f98d019a189709a0910581810d451
MokhaLeee <nkulyc@163.com>
2025-01-14 14:58:33 +0000

    compile

--- sh/shell.c
+++ sh/shell.c
@@ -191,9 +191,11 @@ static void do_init_vars(void)
 
 						LOCAL_INFO("get val(%s)=%s\n", varname, var_val);
 
+#if 0
 						new_var = get_value_from_var(varname);
 						if (new_var)
 							free_var(new_var, shell_vars);
+#endif
 
 						new_var = malloc(sizeof(*new_var));
 						assert(new_var);
