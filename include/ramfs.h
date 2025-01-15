a01d5c8c8904c0dd5b3137969f71fe4d1e1156e8
MokhaLeee <nkulyc@163.com>
2025-01-15 12:08:47 +0800

    compile

--- include/ramfs.h
+++ include/ramfs.h
@@ -95,7 +95,7 @@ void modify_fpath(char *fpath);
 
 #define TRACE_EN 0
 #define INFO_EN  1
-#define ERROR_EN 1
+#define ERROR_EN 0
 
 #define dprintf(prefix, format, ...) printf("\033[35m"prefix"(%s:%d: %s) \033[0m"format, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
 
