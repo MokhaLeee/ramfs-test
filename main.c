8898c472792c374a82bb7d20514b996967a147e3
MokhaLeee <nkulyc@163.com>
2025-01-15 10:59:19 +0800

    compile

--- main.c
+++ main.c
@@ -14,21 +14,6 @@ int main() {
     int fd;
     test(rmkdir, -1, "/000000000000000000000000000000001");
 
-    test(rmkdir, 0, "/it");
-    test(rmkdir, 0, "/it/has");
-    test(rmkdir, 0, "/it/has/been");
-    test(rmkdir, 0, "/it/has/been/a");
-    test(rmkdir, 0, "/it/has/been/a/long");
-
-    succopen(fd, "/it/has/been/a/long", O_CREAT);
-    failopen(fd, "it/has/been/a/long", O_CREAT);
-    char buf[105];
-    test(rread, -1, fd, buf, 100);
-    test(rwrite, -1, fd, "a", 1);
-    test(rrmdir, -1, "/it/has/been");
-    test(rrmdir, 0, "/it/has/been/a/long");
-    test(rwrite, -1, fd, "a", 1);
-    test(rread, -1, fd, buf, 100);
 
     init_shell();
     close_shell();
